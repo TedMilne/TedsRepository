7bdbf2074dd97b9690f2da063e05cc77fa07a4e2
Ted Milne <tmilne@visualstatement.com>
2013-03-17 20:20:28 -0700

    initial commit

---
+++ StringOperations/StringOperations/StringStuff.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringOperations
+{
+    public class StringStuff
+    {
+        public int StringNoSpaceCount(string stringToAnalyze_)
+        {
+            int counter = 0;
+            foreach (char currentChar in stringToAnalyze_)
+            {
+                if (!currentChar.Equals(' '))
+                    counter++;
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ StringOperations/StringStuff.Tests/StringStuffTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringOperations.Tests
+{
+    [TestFixture]
+    public class StringStuffTests
+    {
+        [Test]
+        public void StringNoSpaceCount_EmptyString_ReturnsZero()
+        {
+            
+            StringStuff strStuff = new StringStuff();
+            int result = strStuff.StringNoSpaceCount("");
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void StringNoSpaceCount_SimpleString_ReturnsOne()
+        {
+            StringStuff strStuff = new StringStuff();
+            int result = strStuff.StringNoSpaceCount("a");
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void StringNoSpaceCount_StringWithSpaces_ReturnsSeven()
+        {
+            StringStuff strStuff = new StringStuff();
+            int result = strStuff.StringNoSpaceCount("a    bc   d    e    f                  g");
+            Assert.AreEqual(7, result);
+        }
+    }
+}
